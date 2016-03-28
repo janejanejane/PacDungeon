322297f14187a05357eabc2f2f885e653acf7fdc
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-03-28 22:36:34 +1300

    Update load from browser to electron

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-require( './lib/pixi.js' );
-require( './lib/phaser.js' );
-require( './main.js' );
+'use strict';
+
+// require( './lib/pixi.js' );
+// require( './lib/phaser.js' );
+// require( './main.js' );
+
+const appRootDir = require( 'app-root-dir' );
+const electron = require( 'electron' );
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+// global reference to avoid auto quit when object is garbage collected
+var mainWindow = null;
+
+// quit when all windows are closed
+app.on( 'window-all-closed', function() {
+    if ( process.platform != 'darwin' ) {
+        app.quit();
+    }
+});
+
+// Electron is initialized and ready to create windows
+app.on( 'ready', function() {
+    var file = 'file://' + appRootDir.get() + '/index.html';
+
+    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow.loadURL( file );
+//    mainWindow.webContents.openDevTools();
+    mainWindow.setMenu( null );
+    mainWindow.on( 'close', function() {
+        // multi windows are in an array, dereference when closing each one
+        mainWindow = null;
+    });
+});
