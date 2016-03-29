f51da2dca8b7d9ce9b6069e9467ead69600f7a97
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-03-29 22:52:37 +1300

    Adjust proper index for electron; separate from game index

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const appRootDir = require( 'app-root-dir' );
+const electron = require( 'electron' );
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+// global reference to avoid auto quit when object is garbage collected
+var mainWindow = null;
+
+// quit when all windows are closed
+app.on( 'window-all-closed', function() {
+    if ( process.platform != 'darwin' ) {
+        app.quit();
+    }
+});
+
+// Electron is initialized and ready to create windows
+app.on( 'ready', function() {
+    var file = 'file://' + appRootDir.get() + '/index.html';
+
+    mainWindow = new BrowserWindow({ width: 700, height: 720 });
+    mainWindow.loadURL( file );
+//    mainWindow.webContents.openDevTools();
+    mainWindow.setMenu( null );
+    mainWindow.on( 'close', function() {
+        // multi windows are in an array, dereference when closing each one
+        mainWindow = null;
+    });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,41 @@
 var path = require( 'path' );
 var webpack = require( 'webpack' );
+var webpackRenderer = require( 'webpack-target-electron-renderer' );
 
-module.exports = {
-	cache: true,
-	entry: {
-		main: './src/index.js'
-	},
-	output: {
-		path: path.join( __dirname, 'dist' ),
-		publicPath: 'dist/',
-		filename: '[name].js',
-		chunkFilename: '[chunkhash].js'
-	},
-	module: {
-		loaders: [
-			// required to write 'require('./style.css')'
-			{ test: /\.css$/,    loader: 'style-loader!css-loader' },
+var options = {
+    debug: true,
+    cache: true,
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        path: path.join( __dirname, 'dist' ),
+        publicPath: 'dist/',
+        filename: '[name].js',
+        chunkFilename: '[chunkhash].js'
+    },
+    module: {
+        loaders: [
+            // required to write 'require('./style.css')'
+            { test: /\.css$/,    loader: 'style-loader!css-loader' },
 
-			// required for bootstrap icons
-			{ test: /\.woff$/,   loader: 'url-loader?prefix=font/&limit=5000&mimetype=application/font-woff' },
-			{ test: /\.ttf$/,    loader: 'file-loader?prefix=font/' },
-			{ test: /\.eot$/,    loader: 'file-loader?prefix=font/' },
-			{ test: /\.svg$/,    loader: 'file-loader?prefix=font/' },
+            // required for bootstrap icons
+            { test: /\.woff$/,   loader: 'url-loader?prefix=font/&limit=5000&mimetype=application/font-woff' },
+            { test: /\.ttf$/,    loader: 'file-loader?prefix=font/' },
+            { test: /\.eot$/,    loader: 'file-loader?prefix=font/' },
+            { test: /\.svg$/,    loader: 'file-loader?prefix=font/' },
 
-			{ test: /\.(png|jpg|gif)$/, loader: 'url-loader?limit=8192' },
+            { test: /\.(png|jpg|gif)$/, loader: 'url-loader?limit=8192' },
 
-			{ test: /\.(json|xml)$/, loader: 'file-loader' },
+            { test: /\.(json|xml)$/, loader: 'file-loader' },
 
-			{ test: /pixi.js/, loader: 'script' },
+            { test: /pixi.js/, loader: 'script' },
       { test: /phaser.js/, loader: 'script' }
-		]
-	},
-	plugins: []
+        ]
+    },
+    plugins: []
 };
+
+options.target = webpackRenderer( options );
+
+module.exports = options;
