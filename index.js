8c429c73ac1c5560b79c29c085f1800baf2db5d0
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-03-30 21:27:57 +1300

    Add minimum window size for electron

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const appRootDir = require( 'app-root-dir' );
 const electron = require( 'electron' );
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const WIDTH = 670;
+const HEIGHT = 720;
 
 // global reference to avoid auto quit when object is garbage collected
 var mainWindow = null;
@@ -19,7 +21,8 @@ app.on( 'window-all-closed', function() {
 app.on( 'ready', function() {
     var file = 'file://' + appRootDir.get() + '/index.html';
 
-    mainWindow = new BrowserWindow({ width: 700, height: 720 });
+    mainWindow = new BrowserWindow({ width: WIDTH, height: HEIGHT });
+    mainWindow.setMinimumSize( WIDTH, HEIGHT );
     mainWindow.loadURL( file );
 //    mainWindow.webContents.openDevTools();
     mainWindow.setMenu( null );
